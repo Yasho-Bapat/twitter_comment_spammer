da97fade608e27cf493f2f60415e1ca32d0a9195
Yasho Bapat <93445114+Yasho-Bapat@users.noreply.github.com>
2021-12-12 12:15:23 +0530

    Update twitter_comment_spammer.py

--- twitter_comment_spammer.py
+++ twitter_comment_spammer.py
@@ -1,13 +1,15 @@
 import tweepy
 
-auth = tweepy.OAuthHandler('GROacRrJYa78eeX2ZuJNofuIH', 'UzFd6hcbYwKKNSicu1kt8KhyhoCFV3yPMGuhX4j5pgCzvNXL3u')
-auth.set_access_token('1242846079776722945-crFCmDmOV00gxW7l0c5EnmeBE9lzNB', 'iONwOIvsbqYTJmkQBbm52eiYEDFlyLXdGxh3RdbuDQqAD')
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 user = input("enter user: ")
 comment = input("enter comment phrase: ")
+
 target_tweets = api.user_timeline(screen_name = user, count = 50)
+
 for tweet in target_tweets:
     print(tweet.text)
     api.update_status(status = ("@" + tweet.user.screen_name + " " + comment), in_reply_to_status_id = tweet.id)
